6ce03bd4c63a970530020e416e135a84696a0807
a.kurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-07-01 10:24:08 +0200

    set up project

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/bxcodec/faker/v3 v3.6.0
+	github.com/rollbar/rollbar-go v1.4.1
 	gorm.io/gorm v1.21.11
 	goyave.dev/goyave/v3 v3.10.0
 )
--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"goyave.dev/template/http/route"
 	_ "goyave.dev/template/http/validation"
 
+	"time"
+
+	"github.com/rollbar/rollbar-go"
 	"goyave.dev/goyave/v3"
 	// Import the appropriate GORM dialect for the database you're using.
 	// _ "goyave.dev/goyave/v3/database/dialect/mysql"
@@ -19,4 +22,18 @@ func main() {
 	if err := goyave.Start(route.Register); err != nil {
 		os.Exit(err.(*goyave.Error).ExitCode)
 	}
+
+	rollbar.SetToken("MY_TOKEN")
+	rollbar.SetEnvironment("dev")                                         // defaults to "development"
+	rollbar.SetCodeVersion("v2")                                          // optional Git hash/branch/tag (required for GitHub integration)
+	rollbar.SetServerHost("web.1")                                        // optional override; defaults to hostname
+	rollbar.SetServerRoot("https://github.com/akurtaliqi/goyave-rollbar") // path of project (required for GitHub integration and non-project stacktrace collapsing)
+
+	rollbar.Info("Message body goes here")
+	rollbar.WrapAndWait(doSomething)
+}
+
+func doSomething() {
+	var timer *time.Timer = nil
+	timer.Reset(10) // this will panic
 }
